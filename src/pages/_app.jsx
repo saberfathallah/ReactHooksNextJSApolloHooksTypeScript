d05debfd1e26ce4b8eb553071b80712af60a599e
saber <saberfathallah7@gmail.com>
2020-03-31 17:23:38 +0200

    send token in headers

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,19 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'node-fetch'
 import { ApolloProvider } from '@apollo/react-hooks';
 
+import buildCookies from '../services/cookies';
+import TOKEN_COOKIE from '../constants/cookies';
+
 const client = new ApolloClient({
+  request: operation => {
+    const token = buildCookies().get(TOKEN_COOKIE);
+
+    operation.setContext({
+      headers: {
+        authorization: token || '',
+      },
+    })
+  },
   uri: 'http://localhost:4002/graphql',
   fetch,
 });
