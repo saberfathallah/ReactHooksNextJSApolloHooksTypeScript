ce90bff7dfa00c1a39c47c149776078b39bbf2ff
saber <saberfathallah7@gmail.com>
2020-04-06 17:05:57 +0200

    update comment

--- src/component/comment/Comment.tsx
+++ src/component/comment/Comment.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
+import { Formik } from 'formik';
+import TextField from '@material-ui/core/TextField';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -8,6 +10,7 @@ import EditIcon from '@material-ui/icons/Edit';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import DELETE_COMMENT, { updateCacheAfterDeleteComment } from '../../graphql/comments/mutation/deleteComment';
+import UPDATE_COMMENT, { updateCacheAfterUpdateComment } from '../../graphql/comments/mutation/updateComment';
 
 interface IUser {
   name: string;
@@ -25,6 +28,44 @@ interface ICommetProps {
   currentCategoryId: string;
 }
 
+interface IComment {
+  description: string;
+  userId: IUser;
+  postId: string;
+  id: string;
+}
+
+interface IUpdatedCommetResponse {
+  updateComment: IComment;
+}
+
+interface IDeleteComment {
+  commentId: string;
+  postId: string;
+}
+
+interface IDeletedCommetResponse {
+  deleteComment: IDeleteComment;
+}
+
+interface ICommentEditFormInput {
+  description: string;
+  commentId: string;
+}
+
+interface IFormUpdate {
+  description: string;
+  commentId: string;
+}
+
+interface IUpdateCommentInput {
+  updateCommentInput: IFormUpdate
+}
+
+interface IDeleteCommentInput {
+  deleteCommentInput: IDeleteComment
+}
+
 const Comment: React.FC<ICommetProps> = (props: ICommetProps) => {
   const {
     name,
@@ -37,11 +78,80 @@ const Comment: React.FC<ICommetProps> = (props: ICommetProps) => {
     currentCategoryId
   } = props;
   const [isShown, setIsShown] = React.useState(false);
-  const [deleteComment] = useMutation<any>(DELETE_COMMENT, { 
+  const [isClickEdit, setIsClickEdit] = React.useState(false);
+  const initialValues: ICommentEditFormInput = { description, commentId: id };
+  
+  const [deleteComment] = useMutation<IDeletedCommetResponse, IDeleteCommentInput>(DELETE_COMMENT, { 
     update: (cache, { data }) => updateCacheAfterDeleteComment(cache, data, currentCategoryId) 
+  });
+  const [updateComment] = useMutation<IUpdatedCommetResponse, IUpdateCommentInput>(UPDATE_COMMENT, { 
+    update: (cache, { data }) => updateCacheAfterUpdateComment(cache, data, currentCategoryId) 
   });
 
   return (
+    <>
+    {
+      isClickEdit ?
+      <Formik
+        initialValues={initialValues}
+        validate={values => {
+          let errors = {};
+          if (!values.description) {
+            errors = {
+              ...errors,
+              description: 'Required',
+            }
+          }
+          return errors;
+        }}
+        onSubmit={async values => {
+          if (values.description !== description) {
+            await updateComment({ variables: { updateCommentInput: {
+              ...values,
+              commentId: id,
+            }},
+            })
+          }
+          setIsClickEdit(false)
+        }}
+      >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      }) => (
+        <form style={{ display: "contents" }} onSubmit={handleSubmit}>
+          <TextField
+            type="description"
+            name="description"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.description}
+            id="standard-full-width"
+            label="Ajouter un commentaire"
+            style={{ margin: 8 }}
+            placeholder=""
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          {errors.description && touched.description && errors.description}
+          <IconButton
+            type="submit"
+            onClick={() => setIsClickEdit(true)}
+            aria-label="modify"
+          >
+            <EditIcon />
+          </IconButton>
+        </form>
+      )}
+    </Formik> 
+    : 
     <div style={{ display: "contents" }}
       onMouseEnter={() => setIsShown(true)}
       onMouseLeave={() => setIsShown(false)}
@@ -50,31 +160,34 @@ const Comment: React.FC<ICommetProps> = (props: ICommetProps) => {
         <Avatar alt="Profile Picture" src="" />
       </ListItemAvatar>
       <ListItemText primary={name} secondary={description} />
-      {isShown && 
+      {isShown &&
         <div>
           {
             <> 
-              {
-                ((userConnected.id === userCommentedId) || (userConnected.id === creatorId) ) &&
-                  <IconButton
-                    aria-label="delete"
-                    onClick={() => deleteComment({ variables: { deleteCommentInput: { postId, commentId: id } } })}
-                  >
-                    <DeleteIcon fontSize="small" />
-                  </IconButton>
+              {((userConnected.id === userCommentedId) || (userConnected.id === creatorId) ) &&
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => deleteComment({ variables: { deleteCommentInput: { postId, commentId: id } } })}
+                >
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
               }
-              {
-                userConnected.id === userCommentedId && 
-                  <IconButton aria-label="modify" >
-                    <EditIcon />
-                  </IconButton>
+              {userConnected.id === userCommentedId && 
+                <IconButton
+                  onClick={() =>setIsClickEdit(true)}
+                  aria-label="modify"
+                >
+                  <EditIcon />
+                </IconButton>
               }
             </>
-            
+          
           }
         </div>
       }
     </div>
+    }
+    </>
   );
 } 
   
---
+++ src/graphql/comments/mutation/updateComment.ts
@@ -0,0 +1,72 @@
+import { gql } from 'apollo-boost';
+
+import GET_POSTS_BY_CATEGORY_ID from '../../posts/getPostsByCategoryId';
+import GET_ALL_POSTS from '../../posts/getAllPosts';
+
+const UPDATE_COMMENT = gql`
+mutation updateComment($updateCommentInput: UpdateCommentInput) {
+  updateComment(updateCommentInput:$updateCommentInput) {
+    comment {
+      id
+      postId
+      description
+      userId {
+        name
+        id
+      }
+    }
+    error
+  }
+}
+`;
+
+export const updateCacheAfterUpdateComment = (cache, data, currentCategoryId) => {
+  if (currentCategoryId !== '') {
+    const { getPostsByCategoryId } = cache.readQuery({
+      query: GET_POSTS_BY_CATEGORY_ID,
+      variables: { categoryId: currentCategoryId },
+    });
+    const cloneData = JSON.parse(JSON.stringify(getPostsByCategoryId))
+    const index = cloneData.posts.findIndex(post => post.id === data.updateComment.comment.postId);
+    const comments = cloneData.posts[index].comments.map(comment => {
+      if (comment.id === data.updateComment.comment.id)
+         return {
+           ...comment,
+           description:  data.updateComment.comment.description,
+         }
+        return comment;
+    });
+    cloneData.posts[index].comments = comments;
+    
+    cache.writeQuery({  
+      query: GET_POSTS_BY_CATEGORY_ID,
+      variables: { categoryId: currentCategoryId },
+      data: { getPostsByCategoryId: { ...cloneData, __typename: 'getPostsByCategoryId' } }
+    });
+  }
+
+  const { getAllPosts } = cache.readQuery({
+    query: GET_ALL_POSTS
+  });
+  const cloneAllPosts = JSON.parse(JSON.stringify(getAllPosts))
+  const foundIndex = cloneAllPosts.posts.findIndex(post => post.id === data.updateComment.comment.postId);
+  const comments = cloneAllPosts.posts[foundIndex].comments.map(comment => {
+    if (comment.id === data.updateComment.comment.id)
+       return {
+         ...comment,
+         description:  data.updateComment.comment.description,
+       }
+      return comment;
+  }); 
+  
+  cloneAllPosts.posts[foundIndex].comments = comments;
+
+  cache.writeQuery({  
+    query: GET_ALL_POSTS,
+    data: { getAllPosts: { ...cloneAllPosts, __typename: 'Posts', } }
+  });
+};
+
+export default UPDATE_COMMENT;
+
+
