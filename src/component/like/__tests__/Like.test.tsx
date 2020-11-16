13cc87747912da0090d5398176a205020ce6b603
saber <saberfathallah7@gmail.com>
2020-11-16 14:59:26 +0100

    test like component

--- src/component/like/__tests__/Like.test.tsx
+++ src/component/like/__tests__/Like.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { mount } from "enzyme";
 
 import Like from "../Like";
 
@@ -28,5 +29,55 @@ describe("Like", () => {
 
     const tree = renderer.create(<Like {...props} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should display 0 when nombreOffLikes===null likes snapshot", () => {
+    const props = {
+      dislike: () => {},
+      addLike: () => {},
+      nombreOffLikes: null,
+      isLiked: true,
+      postId: "5e89c6878245cd0d75dec510",
+    };
+
+    const tree = renderer.create(<Like {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it("shouldn't call dislike should call addLike", () => {
+    const addLike = jest.fn();
+    const dislike = jest.fn();
+
+    const props = {
+      dislike: dislike,
+      addLike: addLike,
+      nombreOffLikes: 1,
+      isLiked: false,
+      postId: "5e89c6878245cd0d75dec510",
+    };
+
+    const wrapper = mount(<Like {...props} />);
+    wrapper.find("button").simulate("click");
+    expect(addLike).toHaveBeenCalledTimes(1);
+    expect(dislike).not.toHaveBeenCalled();
+  });
+
+  it("shouldn't call addLike should call dislike", () => {
+    const addLike = jest.fn();
+    const dislike = jest.fn();
+
+    const props = {
+      dislike: dislike,
+      addLike: addLike,
+      nombreOffLikes: 1,
+      isLiked: true,
+      postId: "5e89c6878245cd0d75dec510",
+    };
+
+    const wrapper = mount(<Like {...props} />);
+    wrapper.find("button").simulate("click");
+    expect(dislike).toHaveBeenCalledTimes(1);
+    expect(addLike).not.toHaveBeenCalled();
   });
 });
