b24f7dc2805957a2482f402006a796f8376db482
saber <saberfathallah7@gmail.com>
2020-11-17 12:53:22 +0100

    fix comment test

--- src/component/comment/__tests__/Comment.test.tsx
+++ src/component/comment/__tests__/Comment.test.tsx
@@ -4,19 +4,6 @@ import { mount } from "enzyme";
 import { Formik } from "formik";
 
 import Comment from "../Comment";
-import { useMutation } from "../../../hooks/useMutation";
-import { USER_CONNECTED } from "../../../../mock/usersMock";
-
-jest.mock("../../../hooks/useQuery", () => ({
-  useQuery: jest.fn(),
-}));
-
-jest.mock("../../../hooks/useMutation", () => ({
-  useMutation: jest.fn(),
-}));
-
-// @ts-ignore
-useMutation.mockImplementation(() => [() => {}, { loading: false }]);
 
 describe("Comment", () => {
   const deleteComment = jest.fn();
@@ -24,144 +11,69 @@ describe("Comment", () => {
   const setIsShown = jest.fn();
   const setIsClickEdit = jest.fn();
 
-  it("should match snapshot isShown===true && isClickEdit===true", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: true,
-      isClickEdit: true,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
+  let props = {
+    deleteComment,
+    setIsShown,
+    updateComment,
+    setIsClickEdit,
+    isShown: true,
+    isClickEdit: true,
+    categoryId: "5e89c6458245cd0d75dec50f",
+    name: "commment",
+    description: "description",
+    creatorId: "5e89c2fd8245cd0d75dec501",
+    postId: "5e89c6878245cd0d75dec510",
+    userCommentedId: "5e89c6878245cd0d75dec510",
+    id: "5e89c6878245cd0d75dec510",
+    userConnected: {
       id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
+      email: "sberrr@gmail.com",
+      name: "sberrr",
+    },
+  };
 
+  it("should match snapshot isShown===true && isClickEdit===true", () => {
     const tree = renderer.create(<Comment {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should match snapshot isShown===true && isClickEdit===false display edit and delete icons", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: true,
-      isClickEdit: false,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: {
-        id: "5e89c6878245cd0d75dec510",
-        email: "sberrr@gmail.com",
-        name: "sberrr",
-      },
-    };
+    props.isClickEdit = false;
 
     const tree = renderer.create(<Comment {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should match snapshot isShown===true && isClickEdit===false display delete icon", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: true,
-      isClickEdit: false,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: {
-        id: "5e89c2fd8245cd0d75dec501",
-        email: "sberrr@gmail.com",
-        name: "sberrr",
-      },
-    };
+    props.isClickEdit = false;
+    props.userConnected.id = "5e89c2fd8245cd0d75dec501";
 
     const tree = renderer.create(<Comment {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should match snapshot isShown===false && isClickEdit===true", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: false,
-      isClickEdit: true,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
+    props.isClickEdit = true;
+    props.isShown = false;
 
     const tree = renderer.create(<Comment {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should match snapshot isShown===false && isClickEdit===false", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: false,
-      isClickEdit: false,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
+    props.isClickEdit = false;
+    props.isShown = false;
 
     const tree = renderer.create(<Comment {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should update description field on change", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: false,
-      isClickEdit: true,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
+    props.isClickEdit = true;
+    props.isShown = false;
+
     const wrapper = mount(<Comment {...props} />);
     const descriptionInput = wrapper.find("input");
-
     descriptionInput.simulate("change", {
       persist: () => {},
       target: {
@@ -174,24 +86,10 @@ describe("Comment", () => {
   });
 
   it("should return error for invalid comment", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: true,
-      isClickEdit: true,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
-    const tree = mount(<Comment {...props} />);
+    props.isClickEdit = true;
+    props.isShown = true;
 
+    const tree = mount(<Comment {...props} />);
     const form = (props: any = { errors: {} }) =>
       tree.find(Comment).find(Formik).renderProp("children")(props);
 
@@ -210,24 +108,10 @@ describe("Comment", () => {
   });
 
   it("should call setIsShown", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: false,
-      isClickEdit: false,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: USER_CONNECTED,
-    };
-    const wrapper = mount(<Comment {...props} />);
+    props.isClickEdit = false;
+    props.isShown = false;
 
+    const wrapper = mount(<Comment {...props} />);
     wrapper.find("#comment-actions").simulate("mouseenter");
     expect(setIsShown).toHaveBeenCalledWith(true);
     wrapper.find("#comment-actions").simulate("mouseleave");
@@ -235,32 +119,13 @@ describe("Comment", () => {
   });
 
   it("should call deleteComment", () => {
-    const props = {
-      deleteComment,
-      setIsShown,
-      updateComment,
-      setIsClickEdit,
-      isShown: true,
-      isClickEdit: false,
-      categoryId: "5e89c6458245cd0d75dec50f",
-      name: "commment",
-      description: "description",
-      creatorId: "5e89c2fd8245cd0d75dec501",
-      postId: "5e89c6878245cd0d75dec510",
-      userCommentedId: "5e89c6878245cd0d75dec510",
-      id: "5e89c6878245cd0d75dec510",
-      userConnected: {
-        id: "5e89c2fd8245cd0d75dec501",
-        email: "sberrr@gmail.com",
-        name: "sberrr",
-      },
-    };
+    props.isShown = true;
+
     const wrapper = mount(<Comment {...props} />);
-    
     wrapper.find("button").simulate("click");
     expect(deleteComment).toHaveBeenCalledTimes(1);
   });
-  
+
   // it("should call setIsClickEdit with true", () => {
   //   const props = {
   //     deleteComment,
@@ -285,11 +150,10 @@ describe("Comment", () => {
   //   const wrapper = mount(<Comment {...props} />);
   //   wrapper.find("#IconButtondelete").simulate("click");
 
-    
-    // const button = wrapper.findWhere(
-    //   (node) => node.type() === 'button' && node.find(DeleteIcon)
-    // );
-    // button.simulate("click");
-    // expect(setIsClickEdit).toHaveBeenCalledTimes(1);
+  // const button = wrapper.findWhere(
+  //   (node) => node.type() === 'button' && node.find(DeleteIcon)
+  // );
+  // button.simulate("click");
+  // expect(setIsClickEdit).toHaveBeenCalledTimes(1);
   // });
 });
