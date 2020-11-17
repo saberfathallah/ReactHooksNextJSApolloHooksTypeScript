98e39f6f74c824a5a0abd5960518e05ccf62cd8b
saber <saberfathallah7@gmail.com>
2020-11-17 13:40:48 +0100

    fix postsContainer tests

--- src/component/postsContainer/__tests__/postsContainer.test.tsx
+++ src/component/postsContainer/__tests__/postsContainer.test.tsx
@@ -1,43 +1,75 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { mount } from "enzyme";
 
 import PostsContainer from "../PostsContainer";
-import { useQuery } from "../../../hooks/useQuery";
 import { USER_CONNECTED } from "../../../../mock/usersMock";
+import { POST } from "../../../../mock/postsMock";
+import { useQuery } from "../../../hooks/useQuery";
+import { useMutation } from "../../../hooks/useMutation";
 
 jest.mock("../../../hooks/useQuery", () => ({
   useQuery: jest.fn(),
 }));
 
-// @ts-ignore
-useQuery.mockImplementation(() => ({
-  data: { getUserDetails: { user: USER_CONNECTED }, getPostsByCategoryId: { posts: [] } },
+jest.mock("../../../hooks/useMutation", () => ({
+  useMutation: jest.fn(),
 }));
 
+// @ts-ignore
+useQuery.mockImplementation(() => ({
+  data: {
+    getUserDetails: { user: USER_CONNECTED },
+    getPostsByCategoryId: { posts: [POST] },
+  },
+}));
+
+// @ts-ignore
+useMutation.mockImplementation(() => [() => {}, { loading: false }]);
+
 describe("PostsContainer", () => {
-  it("should display snapshot", () => {
-    const props = {
-      currentCategoryId: "5e89c6028245cd0d75dec50b",
-      data: { getUserDetails: { user: USER_CONNECTED } },
-      dataSearchQuery: { query: "" },
-      loading: false,
-      loadingSearchQuery: false,
-      loadingUserDetails: false,
-    };
+  const props = {
+    currentCategoryId: "5e89c6028245cd0d75dec50b",
+    data: { getUserDetails: { user: USER_CONNECTED } },
+    dataSearchQuery: { query: "" },
+    loading: false,
+    loadingSearchQuery: false,
+    loadingUserDetails: false,
+  };
+
+  it("should match snapshot", () => {
     const tree = renderer.create(<PostsContainer {...props} />);
     expect(tree).toMatchSnapshot();
   });
 
-  it("should display loadiing snapshot", () => {
-    const props = {
-      currentCategoryId: "5e89c6028245cd0d75dec50b",
-      data: { getUserDetails: { user: USER_CONNECTED } },
-      dataSearchQuery: { query: "" },
-      loading: true,
-      loadingSearchQuery: false,
-      loadingUserDetails: false,
-    };
+  it("should display posts by categoryID", () => {
+    const wrapper = mount(<PostsContainer {...props} />);
+    expect(wrapper.text()).toContain("corona france post");
+  });
+
+  it("should display all posts", () => {
+    // @ts-ignore
+    useQuery.mockImplementation(() => ({
+      data: {
+        getUserDetails: { user: USER_CONNECTED },
+        getAllPosts: { posts: [POST], totalPosts: 1 },
+      },
+    }));
+    props.currentCategoryId = "";
+
+    const wrapper = mount(<PostsContainer {...props} />);
+    expect(wrapper.text()).toContain("corona france post");
+  });
+
+  it("should match lading snapshot", () => {
+    props.loading = true;
+
     const tree = renderer.create(<PostsContainer {...props} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should display loading", () => {
+    const wrapper = mount(<PostsContainer {...props} />);
+    expect(wrapper.text()).toContain("laoding....");
   });
 });
