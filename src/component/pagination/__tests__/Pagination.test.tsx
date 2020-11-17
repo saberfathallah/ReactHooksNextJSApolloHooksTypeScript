e7e6528577188fb8ee579c91e1286cdb7ff61634
saber <saberfathallah7@gmail.com>
2020-11-17 12:53:40 +0100

    fix paginations tests

--- src/component/pagination/__tests__/Pagination.test.tsx
+++ src/component/pagination/__tests__/Pagination.test.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { mount } from "enzyme";
+
 import Pagination from "../Pagination";
 
 describe("BrandName", () => {
+  const fetchMorePost = jest.fn();
+
   it("should match snapshot when totalPosts % LIMIT === 0", () => {
     const tree = renderer.create(
-      <Pagination fetchMorePost={() => {}} page={1} totalPosts={10} />
+      <Pagination fetchMorePost={fetchMorePost} page={1} totalPosts={10} />
     );
     expect(tree).toMatchSnapshot();
   });
 
   it("should match snapshot when totalPosts % LIMIT > 0", () => {
     const tree = renderer.create(
-      <Pagination fetchMorePost={() => {}} page={1} totalPosts={11} />
+      <Pagination fetchMorePost={fetchMorePost} page={1} totalPosts={11} />
     );
     expect(tree).toMatchSnapshot();
+  });
+
+  it("should display 3 paginations", () => {
+    const tree = mount(
+      <Pagination fetchMorePost={fetchMorePost} page={1} totalPosts={11} />
+    );
+    expect(tree.text()).toContain("1");
+    expect(tree.text()).toContain("2");
+    expect(tree.text()).toContain("3");
   });
 });
