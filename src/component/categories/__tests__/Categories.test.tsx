305d50e9db00499adc785fb781370d1bcb1eaba0
saber <saberfathallah7@gmail.com>
2020-11-17 12:52:36 +0100

    fix categories tests

--- src/component/categories/__tests__/Categories.test.tsx
+++ src/component/categories/__tests__/Categories.test.tsx
@@ -3,59 +3,42 @@ import renderer from "react-test-renderer";
 import { mount } from "enzyme";
 
 import Categories from "../Categories";
-import { useQuery } from "../../../hooks/useQuery";
-import { useMutation } from "../../../hooks/useMutation";
-import { CATEGORY } from '../../../../mock/categoriesMock';
-
-jest.mock("../../../hooks/useQuery", () => ({
-  useQuery: jest.fn(),
-}));
-
-jest.mock("../../../hooks/useMutation", () => ({
-  useMutation: jest.fn(),
-}));
-
-// @ts-ignore
-useQuery.mockImplementation(() => ({
-  data: { getAllCategoriesQuery: { categories: [CATEGORY] } },
-}));
-// @ts-ignore
-useMutation.mockImplementation(() => [() => {}, { loading: false }]);
+import { CATEGORY } from "../../../../mock/categoriesMock";
 
 describe("Categories", () => {
-  it("should display posts snapshot", () => {
-    const props = {
-      selectCategoryId: () => '',
-      changeCurrentCategory: () => '',
-      isDisplayCategories: true,
-      loading: false,
-      data: {
-        getAllCategoriesQuery: {
-          categories: [CATEGORY],
-        },
+  const selectCategoryId = jest.fn();
+  const changeCurrentCategory = jest.fn();
+  const propsWithCategories = {
+    selectCategoryId,
+    changeCurrentCategory,
+    isDisplayCategories: true,
+    loading: false,
+    data: {
+      getAllCategoriesQuery: {
+        categories: [CATEGORY],
       },
-    };
-    const tree = renderer.create(<Categories {...props} />);
+    },
+  };
+
+  it("should display categories snapshot", () => {
+    const tree = renderer.create(<Categories {...propsWithCategories} />);
     expect(tree).toMatchSnapshot();
   });
 
   it("should display loading snapshot", () => {
     const props = {
-      selectCategoryId: () => '',
-      changeCurrentCategory: () => '',
+      selectCategoryId,
+      changeCurrentCategory,
       isDisplayCategories: true,
       loading: true,
       data: undefined,
     };
-    
+
     const wrapper = mount(<Categories {...props} />);
     expect(wrapper.text()).toContain("loading");
   });
 
   it("should call selectCategoryId when isDisplayCategories false", () => {
-    const selectCategoryId = jest.fn();
-    const changeCurrentCategory = jest.fn();
-
     const props = {
       selectCategoryId,
       changeCurrentCategory,
@@ -67,30 +50,20 @@ describe("Categories", () => {
         },
       },
     };
-    
+
     const wrapper = mount(<Categories {...props} />);
-    wrapper.find('.MuiTreeItem-content').simulate('click')
+    wrapper.find(".MuiTreeItem-content").simulate("click");
     expect(selectCategoryId).toHaveBeenCalledTimes(1);
   });
 
   it("should call selectCategoryId when isDisplayCategories true", () => {
-    const selectCategoryId = jest.fn();
-    const changeCurrentCategory = jest.fn();
-
-    const props = {
-      selectCategoryId,
-      changeCurrentCategory,
-      isDisplayCategories: true,
-      loading: false,
-      data: {
-        getAllCategoriesQuery: {
-          categories: [CATEGORY],
-        },
-      },
-    };
-    
-    const wrapper = mount(<Categories {...props} />);
-    wrapper.find('.MuiTreeItem-content').simulate('click')
+    const wrapper = mount(<Categories {...propsWithCategories} />);
+    wrapper.find(".MuiTreeItem-content").simulate("click");
     expect(changeCurrentCategory).toHaveBeenCalledTimes(1);
+  });
+
+  it("should display categories", () => {
+    const wrapper = mount(<Categories {...propsWithCategories} />);
+    expect(wrapper.text()).toContain("Sport");
   });
 });
