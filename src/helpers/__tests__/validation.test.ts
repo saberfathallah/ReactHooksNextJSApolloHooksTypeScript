a155e24eee3a31c3546960afaf47e5563491a368
saber <saberfathallah7@gmail.com>
2020-11-18 14:13:37 +0100

    validation

--- src/helpers/__tests__/validation.test.ts
+++ src/helpers/__tests__/validation.test.ts
@@ -3,12 +3,36 @@ import validationForm from '../validation';
 describe("validationForm", () => {
   it("should return error description", () => {
     const values = { description: "" };
-
     expect(validationForm(values)).toEqual({ description: "Required" });
   });
-  it("should'tn return error description", () => {
-    const values = { description: "description" };
 
+  it("shouldn't return error description", () => {
+    const values = { description: "description" };
+    expect(validationForm(values)).toEqual({ });
+  });
+
+  it("should return error password", () => {
+    const values = { password: "" };
+    expect(validationForm(values)).toEqual({ password: "Required" });
+  });
+
+  it("shouldn't return error password", () => {
+    const values = { password: "password" };
+    expect(validationForm(values)).toEqual({ });
+  });
+
+  it("should return Required error mail", () => {
+    const values = { email: "" };
+    expect(validationForm(values)).toEqual({ email: "Required" });
+  });
+
+  it("should return invalid error mail", () => {
+    const values = { email: "saber@" };
+    expect(validationForm(values)).toEqual({ email: "Invalid email address" });
+  });
+
+  it("shouldn't return  error mail", () => {
+    const values = { email: "email@email.com" };
     expect(validationForm(values)).toEqual({ });
   });
 });
--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,10 +1,27 @@
+interface Values {
+  email: string;
+  name: string;
+  password: string;
+  description: string;
+}
 const validationForm = (values) => {
-  let errors = {};
-  if (!values.description) {
+  let errors: Partial<Values> = {};
+  if (values.description !== undefined && !values.description) {
     errors = {
       ...errors,
       description: "Required",
     };
+  }
+  if (values.password !== undefined && !values.password) {
+    errors.password = "Required";
+  }
+  if (values.email !== undefined && !values.email) {
+    errors.email = "Required";
+  } else if (
+    values.email !== undefined &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  ) {
+    errors.email = "Invalid email address";
   }
   return errors;
 };
