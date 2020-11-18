d07308f004975cdaf7d0ff0e2e2dd8b9a1da0f25
saber <saberfathallah7@gmail.com>
2020-11-18 15:32:34 +0100

    test nex post

--- src/component/newPost/__tests__/NewPost.test.tsx
+++ src/component/newPost/__tests__/NewPost.test.tsx
@@ -26,6 +26,9 @@ useQuery.mockImplementation(() => ({
 // @ts-ignore
 useMutation.mockImplementation(() => [() => {}, { loading: false }]);
 
+const wait = (amount = 0) =>
+  new Promise((resolve) => setTimeout(resolve, amount));
+
 describe("NewPost", () => {
   const setCategoryId = jest.fn();
   const addPost = jest.fn();
@@ -41,7 +44,6 @@ describe("NewPost", () => {
     handleClose,
   };
   it("should  snapshot open===false", () => {
-
     const tree = renderer.create(<NewPost {...props} />);
     expect(tree).toMatchSnapshot();
   });
@@ -113,12 +115,50 @@ describe("NewPost", () => {
     expect(handleClose).toHaveBeenCalledTimes(1);
   });
 
-  // it("should call handleClose", () => {
-  //   const wrapper = mount(<NewPost {...props} />);
-  //   const button = wrapper.findWhere(
-  //     (node) => node.is(Button) && node.text() === "Ajouter"
-  //   );
-  //   button.simulate("click");
-  //   expect(addPost).toHaveBeenCalledTimes(1);
-  // });
+  it("should call addPost and handleClose", async () => {
+    const handleClose = jest.fn();
+    props.handleClose = handleClose;
+    props.categoryId = "categoryId";
+    const wrapper = mount(<NewPost {...props} />);
+    wrapper.find("input[type='description']").simulate("change", {
+      target: {
+        name: "description",
+        value: "description",
+      },
+    });
+
+    const button = wrapper.findWhere(
+      (node) => node.is(Button) && node.text() === "Ajouter"
+    );
+    button.simulate("click");
+    await wait(200);
+
+    expect(addPost).toHaveBeenCalledTimes(1);
+    expect(handleClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("shouldn't call addPost handleClose", async () => {
+    const addPost = jest.fn();
+    const handleClose = jest.fn();
+
+    props.addPost = addPost;
+    props.handleClose = handleClose;
+    props.categoryId = "";
+    const wrapper = mount(<NewPost {...props} />);
+    wrapper.find("input[type='description']").simulate("change", {
+      target: {
+        name: "description",
+        value: "description",
+      },
+    });
+
+    const button = wrapper.findWhere(
+      (node) => node.is(Button) && node.text() === "Ajouter"
+    );
+    button.simulate("click");
+    await wait(200);
+
+    expect(addPost).toHaveBeenCalledTimes(0);
+    expect(handleClose).toHaveBeenCalledTimes(0);
+  });
 });
