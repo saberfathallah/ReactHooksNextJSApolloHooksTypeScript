53a5e7f1ff826534dc0af329b07b404b4fbd051b
saber <saberfathallah7@gmail.com>
2020-12-08 16:47:16 +0100

    test scss

--- src/component/TestSCSS/TestSCSS.tsx
+++ src/component/TestSCSS/TestSCSS.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./testSCSS.scss";
 
 const TestSCSS = () => {
+  const [disable, setDisable] = useState(false);
   return (
     <div>
+      <button
+        onClick={() => setDisable(true)}
+        disabled={disable}
+        className="button"
+      >
+        button
+      </button>
+      <button className="mixin-button">mixin-button</button>
       <p className="text">test scss</p>
     </div>
   );
