1e1b53703b5daa67788ffed14fd72becefb137db
saber <saberfathallah7@gmail.com>
2021-02-04 17:38:37 +0100

    use intervale

---
+++ src/hooks/useInervale.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef }  from 'react';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      // @ts-ignore
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default useInterval;
+
